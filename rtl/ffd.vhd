69edb691ad112a36406afcd1442127415991cc58
rmillon <rmillon@undec.edu.ar>
2026-03-10 15:36:13 -0300

    add ffd.vhd file

---
+++ rtl/ffd.vhd
@@ -0,0 +1,29 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+
+--Declara la interfaz del bloque
+entity ffd is
+  port (
+    d : in std_logic;
+    clk : in std_logic;
+    rst : in std_logic;
+    q : out std_logic;
+    nq : out std_logic
+    );
+end entity ffd;
+
+--Describe la funcionalidad del bloque
+architecture beh of ffd is
+begin
+    process(clk,rst)--Asincronico
+    begin
+        if rst = '1' then --mayor prioridad
+            q <= '0';
+        elsif rising_edge(clk) then
+            q <= d;
+        end if;
+    end process
+
+end architecture;
+
