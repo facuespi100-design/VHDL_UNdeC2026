f1ed90548d60bd22a97c9dbdaac300645af19cd7
rmillon <rmillon@undec.edu.ar>
2026-04-01 16:57:33 -0300

    divfrec compilando

--- rtl/divfrec.vhd
+++ rtl/divfrec.vhd
@@ -13,8 +13,25 @@ end entity divfrec;
 
 --Describe la funcionalidad del bloque
 architecture beh of divfrec is
+   signal contador : unsigned(31 downto 0);
+   constant const1HZ : integer := 100000000/2;
+   signal data_out_aux : std_logic;
+   --constant c100MHz : integer := 2;
 begin
-    
-
+   clk_1hz <= data_out_aux;
+   process(rst, clk) 
+   begin
+       if rst = '1' then
+           data_out_aux <= '0';
+           contador <= 0;
+       elsif rising_edge(clk) then
+            if contador = const1HZ then
+                data_out_aux <= not data_out_aux;
+                contador <= 0;
+            else
+                contador <= contador + 1;
+            end if;
+        end if;
+    end process;
 end architecture;
 
